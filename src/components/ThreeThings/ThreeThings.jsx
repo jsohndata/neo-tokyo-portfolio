b4a0305c74c481cc06e9425115cb058e73fb64a2
jSohnData <jihos@me.com>
2023-04-30 19:48:24 -0400

    ADD id for 3 things

--- src/components/ThreeThings/ThreeThings.jsx
+++ src/components/ThreeThings/ThreeThings.jsx
@@ -4,28 +4,28 @@ export default function ThreeThings() {
 
   return (
     <main>
-      <Container fluid className="three-things-container">
+      <Container fluid className="three-things-container" id="three-things">
         <Row>
-          <Col><h2 className="text-center">Three Skills I'm Grateful For</h2></Col>
+          <Col><h2 className="text-center">Three Skills <span className="d-block d-md-inline">I'm Grateful For</span></h2></Col>
         </Row>
         
         <Row className="justify-content-center g-5">
           <Col sm={8} md={3}>
             <p className="icon">🤗</p>
             <h3 className="text-center">Collaborative Catalyst</h3>
-            <p>I am a team player who brings professional expertise, a positive attitude, and humor to keep the team energized, motivated, while delivering results.</p>
+            <p className="text-center text-md-start">I am a team player who brings professional expertise, a positive attitude, and humor to keep the team energized, motivated, while delivering results.</p>
           </Col>
 
-          <Col md={3}>
+          <Col sm={8} md={3}>
             <p className="icon">🚀</p>
             <h3 className="text-center">Rocketing to Success</h3>
-            <p>With 20+ years in tech, I have found that motivating my team to excel is like launching a rocket ship - challenging, but incredibly rewarding!</p>
+            <p className="text-center text-md-start">With 20+ years in tech, I have found that motivating my team to excel is like launching a rocket ship - challenging, but incredibly rewarding!</p>
           </Col>
 
-          <Col md={3}>
+          <Col sm={8} md={3}>
             <p className="icon">🥪</p>
             <h3 className="text-center">Functionality + Design</h3>
-            <p>Combining tech and design is my jam - like PB&J. I strive to create a seamless, irresistible experience for users whilst keeping things crisp and fresh.</p>
+            <p className="text-center text-md-start">Combining tech and design is my jam - like PB&J. I strive to create a seamless, irresistible experience for users whilst keeping things crisp and fresh.</p>
           </Col>
 
       
