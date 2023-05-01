57c11929fd07c7c2bbea90c80d1b3ea358e61d78
jSohnData <jihos@me.com>
2023-04-30 20:25:34 -0400

    UPDATED css

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -11,8 +11,8 @@ export default function Intro() {
             md={{ span: 7, offset: 5 }} 
             lg={{ span: 6, offset: 4 }} 
             xl={5} 
-            className="about-col">
-              <h1><span>Hello, I am</span> Jiho Sohn</h1>
+            className="p-4">
+              <h1><span>Hello, my name is</span> Jiho Sohn</h1>
               <p>I am a software engineer residing in south Florida. I love JavaScript, React.js, CSS, Python, MongoDB and more. When I'm not crafting code, you will find me tending to my garden, keeping my Belgian Malinois on her toes (paws), and revving up my Ducati like it's an API.</p>
               <HashLink to="/#portfolio"><Button>See Portfolio</Button></HashLink>
           </Col>
--- src/components/ThreeThings/ThreeThings.jsx
+++ src/components/ThreeThings/ThreeThings.jsx
@@ -12,19 +12,19 @@ export default function ThreeThings() {
         <Row className="justify-content-center g-5">
           <Col sm={8} lg={3}>
             <p className="icon">🤗</p>
-            <h3 className="text-center">Collaborative Catalyst</h3>
+            <h3 className="text-center">Collaborative <span className="d-md-block">Catalyst</span></h3>
             <p className="text-center text-md-start">I am a team player who brings professional expertise, a positive attitude, and humor to keep the team energized, motivated, while delivering results.</p>
           </Col>
 
           <Col sm={8} lg={3}>
             <p className="icon">🚀</p>
-            <h3 className="text-center">Rocketing to Success</h3>
+            <h3 className="text-center">Rocket <span className="d-md-block">to Success</span></h3>
             <p className="text-center text-md-start">With 20+ years in tech, I have found that motivating my team to excel is like launching a rocket ship - challenging, but incredibly rewarding!</p>
           </Col>
 
           <Col sm={8} lg={3}>
             <p className="icon">🥪</p>
-            <h3 className="text-center">Functionality + Design</h3>
+            <h3 className="text-center">Functionality <span className="d-md-block">+ Design</span></h3>
             <p className="text-center text-md-start">Combining tech and design is my jam - like PB&J. I strive to create a seamless, irresistible experience for users whilst keeping things crisp and fresh.</p>
           </Col>
 
